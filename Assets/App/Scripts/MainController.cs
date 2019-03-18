5af46ff22e6a596d828ce4d4f4a3337f05d6650d
Mauricio Siller <mauriciosiller@gmail.com>
2019-03-18 17:40:00 -0600

    add call to get logged user's items

---
+++ Assets/App/Scripts/MainController.cs
@@ -0,0 +1,41 @@
+﻿using Proyecto26;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+    WebController wb;
+    List<AppTransitionItem> itemList = new List<AppTransitionItem>();
+
+    List<AppTransitionItem> items = new List<AppTransitionItem>(); 
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.wb = GameObject.Find("WebController").GetComponent<WebController>();
+        this.GetItems();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Get current player items
+    private void GetItems()
+    {
+        string itemsURL = this.wb.GetItemsURL();
+        RestClient.Get(itemsURL).Then(response =>
+        {
+            string jsonComplete = "{\"items\": " + response.Text + "}";
+            var res = JsonUtility.FromJson<AppTransitionItemObject<AppTransitionItem>>(jsonComplete);
+
+            for (var i = 0; i < res.items.Length; i++)
+            {
+                itemList.Add(res.items[i]);
+            }
+        });
+    }
+}
--- Assets/App/Scripts/WebController.cs
+++ Assets/App/Scripts/WebController.cs
@@ -8,6 +8,9 @@ using UnityEngine.Events;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+using System.Threading;
+using System.Threading.Tasks;
+
 
 // ITEM STRUCTURES : BEGIN
 [Serializable]
@@ -99,26 +102,6 @@ public class WebController : MonoBehaviour
         {
             this.baseURL = "https://amulet-api.herokuapp.com/api/";
         }
-
-        //string json = "[{\"id\":\"5c72ec27168727249049f1cf\",\"item_name\":\"Sun\",\"desc\":\"Bright!\",\"location\":{ \"lat\":1.5,\"lng\":2.5}},{\"id\":\"5c72ec45168727249049f1d0\",\"item_name\":\"Moon\",\"desc\":\"Have a rabbit\",\"location\":{\"lat\":2.5,\"lng\":3.5}}]";
-        /*
-        RestClient.Get(itemsRoute).Then(response => {
-            string jsonComplete = "{\"items\": " + response.Text + "}";
-            var res = JsonUtility.FromJson<AppTransitionItemObject<AppTransitionItem>>(jsonComplete);
-            for (var i = 0; i < res.items.Length; i++)
-            {
-                if (res.items[i].model_id == 1)
-                {
-                    Instantiate(this.Moon, new Vector3(res.items[i].location.lat, 0, res.items[i].location.lng), Quaternion.identity);
-                }
-                if (res.items[i].model_id == 2)
-                {
-                    Instantiate(this.Sun, new Vector3(res.items[i].location.lat, 0, res.items[i].location.lng), Quaternion.identity);
-                }
-                Debug.Log(res.items[0].location.lat);
-            }
-         });
-         */
     }
 
     // Update is called once per frame
@@ -138,5 +121,10 @@ public class WebController : MonoBehaviour
         }, error => {
             Debug.Log("Login Error");
         });
+    }
+
+    public string GetItemsURL()
+    {
+        return this.baseURL + "TempUsers/" + this.userId + "/item-actives";
     }
 }
