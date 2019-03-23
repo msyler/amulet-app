b3fa3cb200a6eb8fa09ecfea85eb6fe17e8224ca
Mauricio Siller <mauriciosiller@gmail.com>
2019-03-23 01:08:03 -0600

    feat and fix: add inventory panels and fix forms rendering

---
+++ Assets/App/Scripts/MainUIController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainUIController : MonoBehaviour
+{
+    public GameObject panelInventory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void toggleInventory() {
+        Debug.Log("Click Toggle");
+        bool isActive = panelInventory.activeSelf;
+        panelInventory.SetActive(!isActive);
+    }
+}
