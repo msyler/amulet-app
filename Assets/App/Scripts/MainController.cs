d254da8abea39cf2fa14e57b9f5a7e156a848c14
Mauricio Siller <mauriciosiller@gmail.com>
2019-03-27 23:32:39 -0600

    add inventoty update for new items

--- Assets/App/Scripts/MainController.cs
+++ Assets/App/Scripts/MainController.cs
@@ -2,11 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainController : MonoBehaviour
 {
     WebController wb;
     List<AppTransitionItem> itemList = new List<AppTransitionItem>();
+    public Transform InventSlotPrefab;
+    public Transform InventSlotPrefabParent;
 
     List<AppTransitionItem> items = new List<AppTransitionItem>(); 
     // Start is called before the first frame update
@@ -27,6 +30,7 @@ public class MainController : MonoBehaviour
     private void GetItems()
     {
         string itemsURL = this.wb.GetItemsURL();
+        Debug.Log(itemsURL);
         RestClient.Get(itemsURL).Then(response =>
         {
             string jsonComplete = "{\"items\": " + response.Text + "}";
@@ -35,6 +39,11 @@ public class MainController : MonoBehaviour
             for (var i = 0; i < res.items.Length; i++)
             {
                 itemList.Add(res.items[i]);
+                Debug.Log(res.items[i].item_name);
+                Transform _slotPrefab = Instantiate(InventSlotPrefab, new Vector3(0,0,0), Quaternion.identity);
+                Text txt = _slotPrefab.Find("Text").GetComponent<Text>();
+                txt.text = res.items[i].item_name;
+                _slotPrefab.parent = InventSlotPrefabParent;
             }
         });
     }
