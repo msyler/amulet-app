e14657e047b10b9993af20befdc1b0bdc0a5135b
Mauricio Siller <mauriciosiller@gmail.com>
2019-04-09 22:26:06 -0500

    feat(camera): add smooth camera follow

---
+++ Assets/App/Scripts/Camera/MainCameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour
+{
+
+    public Transform TargetToFollow;
+    public float smoothSpeed = 0.125f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = new Vector3(TargetToFollow.position.x, transform.position.y, TargetToFollow.position.z);
+        transform.position = Vector3.Lerp(transform.position, newPosition, smoothSpeed ) ;
+    }
+}
